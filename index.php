5aef3cabd443b83cd60c8418acaeb9f329d7d810
insipthus <6421061015@cdti.ac.th>
2023-06-14 16:14:45 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="style.css"/>
+
+<?php
+$name = "คุณพี่มีกระไรฤา";
+$age = 18;
+$year = 2566;
+$byear = $year-$age;
+
+echo "<h1> ยินดีต้อนรับสู่เว็ปไซต์สักที่ในสุโขทัย </h1>";
+echo " จัดทำโดย $name </br>";
+echo "พ.ศ. $byear ปีเกิดคุณท่านนะเจ้าคะ";
+?>
+<p class="f44">
+     เว็บไซต์นี้ทำขึ้นเพื่อให้คุณพี่ทดสอบเขียนวิชาโปรแกรมคอมพิวเตอร์เจ้าค่ะ
+    
+</p>
+<img src="images1/pic1.jpg" class="img1"height="300px"/>
+<img src="images1/pic2.jpg" class="img2"height="300px"/>
+
---
+++ login.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="style.css"/>
+
+<h2 id="box1">  เข้าสู่ระบบ  </h2>
+<form method="post" action="checklogin.php" enctype="multipart/form-data">
+    Username :
+    <input name="username" type="text" /> <br/> <br/>
+    Password :
+    <input name="password" type="password" /> <br/> <br/>
+    <input type="submit" value=" LOGIN " />
+</form>
+
+<a href="register.php"> ยังไม่สมัครสมาชิก </a>
